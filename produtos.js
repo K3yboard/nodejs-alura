c48ac3449d4f5883b8040552e3189bc176606c30
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-04-22 20:23:04 -0300

    NodeJs - Criando servidor

---
+++ produtos.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var porta = 3000;
+var ip = 'localhost';
+
+var server = http.createServer(function(req, res) {
+    console.log('Recebendo request');
+    res.writeHead(200, { 'Content-Type': 'text/html'});
+    res.end('<html><body>listando os produtos</body></html>');
+});
+
+server.listen(porta, ip);
+
+console.log('Servidor rodando em http://' + ip + ':' + porta + '/');
