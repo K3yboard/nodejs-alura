d55f2bd41a18c27c7be788938ce64416b9e7d8c6
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-04-23 18:26:29 -0300

    Exibindo dados da tabela e refatorando a classe da connection

--- casa-do-codigo/app/routes/produtos.js
+++ casa-do-codigo/app/routes/produtos.js
@@ -1,18 +1,11 @@
+var dbConnection = require('../infra/connectionFactory');
+
 module.exports = function(app) {
     app.get('/produtos', function(req, res) {
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: '1234qwer',
-            database: 'casadocodigo_nodejs'
-        });
+        var connection = dbConnection();
 
-        connection.query('select * from produtos', function(err, results) {
-            res.send(results);
-            if(err) {
-                console.log(err);
-            }
+        connection.query('select * from produtos', function(erros, resultados) {
+            res.render('produtos/lista', { lista: resultados });
         });
 
         connection.end();
