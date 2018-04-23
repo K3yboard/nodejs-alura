3bfe80fc52132a66de0e31ad5c9772da6a23466f
Paulo Pereira dos Anjos <paulopereiradosanjos@gmail.com>
2018-04-23 16:05:29 -0300

    Criando e configurando o arquivo de rotas

--- casa-do-codigo/app.js
+++ casa-do-codigo/app.js
@@ -1,8 +1,5 @@
 var app = require('./config/express')();
-
-app.get('/produtos', function(request, response) {
-    response.render('produtos/lista');
-});
+var rotasProdutos = require('./app/routes/produtos')(app);
 
 app.listen(3000, function() {
     console.log('Servidor rodando na porta 3000');
